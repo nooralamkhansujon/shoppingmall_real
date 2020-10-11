710615bdebab4427eea4ac72f9b54bca6ccd5ca3
nooralamkhansujon <fahimkhanmpi373950@gmail.com>
2020-10-11 03:06:59 -0700

    i add fillable property,category relationship in this model

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['category_id','product_name','product_code','product_color','description','price','image','status'];
+
+    public function category(){
+        return $this->belongsTo(Category::class,'category_id','id');
+    }
 }
