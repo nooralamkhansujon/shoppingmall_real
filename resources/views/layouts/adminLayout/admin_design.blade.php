8a92006fdca210617cc152dfa706c407ebc54a7a
nooralamkhansujon <fahimkhanmpi373950@gmail.com>
2020-10-11 02:58:33 -0700

    I add sweet alert library in admin desgin layout

--- resources/views/layouts/adminLayout/admin_design.blade.php
+++ resources/views/layouts/adminLayout/admin_design.blade.php
@@ -4,6 +4,7 @@
 <title>Laravel Admin</title>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="stylesheet" href="{{ asset('css/backend_css/bootstrap.min.css') }}" />
 <link rel="stylesheet" href="{{ asset('css/backend_css/bootstrap-responsive.min.css') }}" />
 <link rel="stylesheet" href="{{ asset('css/backend_css/uniform.css') }}" />
@@ -11,7 +12,7 @@
 <link rel="stylesheet" href="{{ asset('css/backend_css/fullcalendar.css') }}" />
 <link rel="stylesheet" href="{{ asset('css/backend_css/matrix-style.css') }}" />
 <link rel="stylesheet" href="{{ asset('css/backend_css/matrix-media.css') }}" />
-<link href="font-awesome/{{ asset('fonts/backend_fonts/css/font-awesome.css') }}" rel="stylesheet" />
+<link href="{{ asset('fonts/backend_fonts/css/font-awesome.css') }}" rel="stylesheet" />
 <link rel="stylesheet" href="{{ asset('css/backend_css/jquery.gritter.css') }}" />
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
 </head>
@@ -25,14 +26,16 @@
 
 @include('layouts.adminLayout.admin_footer')
 
-<script src="{{ asset('js/backend_js/jquery.min.js') }}"></script> 
-<script src="{{ asset('js/backend_js/jquery.ui.custom.js') }}"></script> 
-<script src="{{ asset('js/backend_js/bootstrap.min.js') }}"></script> 
-<script src="{{ asset('js/backend_js/jquery.uniform.js') }}"></script> 
-<script src="{{ asset('js/backend_js/select2.min.js') }}"></script> 
+<script src="{{ asset('js/backend_js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/backend_js/sweetalert2.js') }}"></script>
+<script src="{{ asset('js/backend_js/axios.min.js') }}"></script>
+<script src="{{ asset('js/backend_js/jquery.ui.custom.js') }}"></script>
+<script src="{{ asset('js/backend_js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/backend_js/jquery.uniform.js') }}"></script>
+<script src="{{ asset('js/backend_js/select2.min.js') }}"></script>
 <script src="{{ asset('js/backend_js/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('js/backend_js/jquery.validate.js') }}"></script> 
-<script src="{{ asset('js/backend_js/matrix.js') }}"></script> 
+<script src="{{ asset('js/backend_js/jquery.validate.js') }}"></script>
+<script src="{{ asset('js/backend_js/matrix.js') }}"></script>
 <script src="{{ asset('js/backend_js/matrix.form_validation.js') }}"></script>
 <script src="{{ asset('js/backend_js/matrix.tables.js') }}"></script>
 <script src="{{ asset('js/backend_js/matrix.popover.js') }}"></script>
