1e449442f277e0927aa438d9f68528a52bb9f0d1
nooralamkhansujon <fahimkhanmpi373950@gmail.com>
2020-10-11 03:06:26 -0700

    i add parentCategory,categories relationship in this model

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+     protected $fillable = ['parent_id','name','description','url','status'];
+
+     public function Categories(){
+         return $this->hasMany(Category::class,'parent_id','id');
+     }
+
+     public function parentCategory(){
+         return $this->belongsTo(Category::class,'parent_id','id');
+     }
 }
