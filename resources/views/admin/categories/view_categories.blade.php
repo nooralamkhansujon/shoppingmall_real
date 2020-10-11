d9b1c884c23f6272cc8756fe678bd191b0c4c4c7
nooralamkhansujon <fahimkhanmpi373950@gmail.com>
2020-10-11 02:59:24 -0700

    i show category in viewcategories views using datatable

--- resources/views/admin/categories/view_categories.blade.php
+++ resources/views/admin/categories/view_categories.blade.php
@@ -7,16 +7,16 @@
     <h1>Categories</h1>
      @if(Session::has('flash_message_error'))
         <div class="alert alert-error alert-block">
-            <button type="button" class="close" data-dismiss="alert">×</button> 
+            <button type="button" class="close" data-dismiss="alert">×</button>
                 <strong>{!! session('flash_message_error') !!}</strong>
         </div>
-    @endif   
+    @endif
     @if(Session::has('flash_message_success'))
         <div class="alert alert-success alert-block">
-            <button type="button" class="close" data-dismiss="alert">×</button> 
+            <button type="button" class="close" data-dismiss="alert">×</button>
                 <strong>{!! session('flash_message_success') !!}</strong>
         </div>
-    @endif   
+    @endif
   </div>
   <div class="container-fluid">
     <hr>
@@ -42,12 +42,14 @@
                 <tr class="gradeX">
                   <td>{{ $category->id }}</td>
                   <td>{{ $category->name }}</td>
-                  <td>{{ $category->parent_id }}</td>
+                  <td>{{ $category->parentCategory->name ?? "Main Category" }}</td>
                   <td>{{ $category->url }}</td>
-                  <td class="center"><a href="{{ url('/admin/edit-category/'.$category->id) }}" class="btn btn-primary btn-mini">Edit</a> <a id="delCat" href="{{ url('/admin/delete-category/'.$category->id) }}" class="btn btn-danger btn-mini">Delete</a></td>
+                  <td class="center">
+                      <a href="{{ url('/admin/edit-category/'.$category->id) }}" class="btn btn-primary btn-mini">Edit</a>
+                  <a rel="{{$category->id}}" rel1="delete-category" href="javascript:void(0)" class="btn btn-danger btn-mini deleteRecord">Delete</a></td>
                 </tr>
                 @endforeach
-                
+
               </tbody>
             </table>
           </div>
