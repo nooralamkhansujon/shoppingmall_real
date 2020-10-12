7d1a6e6e091fc3a1ac537143224540bb2a83191f
nooralamkhansujon <fahimkhanmpi373950@gmail.com>
2020-10-12 07:41:12 -0700

    Indexcontroller created and index function created

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+use App\Category;
+class IndexController extends Controller
+{
+    public function index(Request $request){
+        $allProducts = Product::inRandomOrder()->get();
+        //
+        $categories = $this->categories();
+        return view('index',compact('allProducts','categories'));
+    }
+}
