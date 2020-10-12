4833337169855b3973c6a69364d5469d4bdf3628
nooralamkhansujon <fahimkhanmpi373950@gmail.com>
2020-10-12 08:00:11 -0700

    for adding new  category this view is used

--- resources/views/admin/categories/add_category.blade.php
+++ resources/views/admin/categories/add_category.blade.php
@@ -3,8 +3,24 @@
 
 <div id="content">
   <div id="content-header">
-    <div id="breadcrumb"> <a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> <a href="#">Categories</a> <a href="#" class="current">Add Category</a> </div>
-    <h1>Categories</h1>
+  <div id="breadcrumb">
+      <a href="{{route('admin.dashboard')}}" title="Go to Dashboard" class="tip-bottom"><i class="icon-home"></i> Home</a>
+      <a href="{{route('admin.viewCategories')}}">Categories</a>
+      <a href="#" class="current" disabled>Add Category</a>
+   </div>
+      <h1>Categories</h1>
+      @if(Session::has('flash_message_error'))
+        <div class="alert alert-error alert-block">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong>{!! session('flash_message_error') !!}</strong>
+        </div>
+      @endif
+      @if(Session::has('flash_message_success'))
+        <div class="alert alert-success alert-block">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong>{!! session('flash_message_success') !!}</strong>
+        </div>
+      @endif
   </div>
   <div class="container-fluid"><hr>
     <div class="row-fluid">
@@ -14,7 +30,8 @@
             <h5>Add Category</h5>
           </div>
           <div class="widget-content nopadding">
-            <form class="form-horizontal" method="post" action="{{ url('/admin/add-category') }}" name="add_category" id="add_category" novalidate="novalidate"> {{ csrf_field() }}
+            <form class="form-horizontal" method="post" action="{{ route('admin.addCategory') }}" name="add_category" id="add_category" novalidate="novalidate">
+                @csrf
               <div class="control-group">
                 <label class="control-label">Category Name</label>
                 <div class="controls">
