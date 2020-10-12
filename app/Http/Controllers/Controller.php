75439c9de41d6416c77a8f9b2a3d308d414cf81d
nooralamkhansujon <fahimkhanmpi373950@gmail.com>
2020-10-12 07:51:18 -0700

    categories function created here

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+use App\Category;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function categories(){
+        return  Category::with('Categories')->where('parent_id',0)->get();
+    }
 }
