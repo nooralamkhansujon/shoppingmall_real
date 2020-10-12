611821cb8939a1c546f14785443528e6e3ec086d
nooralamkhansujon <fahimkhanmpi373950@gmail.com>
2020-10-12 07:58:28 -0700

    I add view share function for sharing maincategories in every view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Category;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $mainCategories = Category::where('parent_id',0)->get();
+        View::share('mainCategories',$mainCategories);
     }
 }
