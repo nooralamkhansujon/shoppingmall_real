835e3f142359937fe4a7361689bec9f2658248b5
nooralamkhansujon <fahimkhanmpi373950@gmail.com>
2020-10-12 08:00:59 -0700

    I add functionality for url to make it unique

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,8 +9,13 @@ class CategoryController extends Controller
 {
     public function addCategory(Request $request){
     	if($request->isMethod('post')){
-    		$data = $request->all();
-    		//echo "<pre>"; print_r($data); die;
+            $data = $request->all();
+            if(!empty($request->url)){
+                $url_exists = Category::where('url',$request->url)->count();
+                if($url_exists > 0 ){
+                    return redirect()->back()->with('flash_message_error',"Catgory Url should be unique");
+                }
+            }
     		$category = new Category;
     		$category->name        = $data['category_name'];
             $category->parent_id   = $data['parent_id'];
@@ -21,7 +26,6 @@ class CategoryController extends Controller
     	}
 
         $levels = Category::where(['parent_id'=>0])->get();
-
     	return view('admin.categories.add_category')->with(compact('levels'));
     }
 
@@ -29,18 +33,16 @@ class CategoryController extends Controller
 
         if($request->isMethod('post')){
             $data = $request->all();
-
             Category::where(['id'=>$categoryId])->update(['name'=>$data['category_name'],'description'=>$data['description'],'url'=>$data['url'],'parent_id'=>$data['parent_id']]);
-            return redirect('/admin/view-categories')->with('flash_message_success','Category updated Successfully!');
+            return redirect()->route('admin.viewCategories')->with('flash_message_success','Category updated Successfully!');
         }
         $categoryDetails = Category::where(['id'=>$categoryId])->first();
         $levels = Category::where([['parent_id','=',0],['id','<>',$categoryDetails->id]])->get();
-        return view('admin.categories.edit_category')->with(compact('categoryDetails','levels'));
+        return view('admin.categories.edit_category',compact('categoryDetails','levels'));
     }
 
     public function deleteCategory(Request $request, $categoryId = null){
         $category       = Category::find($categoryId);
-
         if($category->delete()){
             return response()->json('Category has been Deleted !');
         }
@@ -50,7 +52,6 @@ class CategoryController extends Controller
     }
 
     public function viewCategories(){
-
     	$categories = Category::with('parentCategory')->get();
     	return view('admin.categories.view_categories',compact('categories'));
     }
