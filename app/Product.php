457eec3520f595902d8a2dcc9d00bc2017c9e49e
nooralamkhansujon <fahimkhanmpi373950@gmail.com>
2020-10-14 10:51:39 -0700

    I add altImges relationship and getStatusAttribute accessor in product model

--- app/Product.php
+++ app/Product.php
@@ -6,14 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['category_id','product_name','product_code','product_color','description','price','image','status'];
+    protected $fillable = ['category_id','product_name','product_code','product_color','description','price','image','status','care'];
 
     public function category(){
         return $this->belongsTo(Category::class,'category_id','id');
     }
 
-    public function Attributes(){
+    public function attributes(){
         return $this->hasMany(ProductsAttribute::class,'product_id','id');
+    }
+
+    public function altImages(){
+        return $this->hasMany(ProductsImage::class,'product_id','id');
+    }
+
+    public function getStatusAttribute($value){
+        return $value == 1?"Active":"InActive";
     }
 
 
